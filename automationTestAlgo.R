50e586a7cccf0d43aec62af42272c4abd8d1b1dd
RUIMY <benjamin.ruimy@ensimag.grenoble-inp.fr>
2017-03-03 09:35:07 +0100

    Ajout du tableau qui calcul l'erreur quadratique et du vecteur de temps de calcul moyen de chaque modèle

--- automationTestAlgo.R
+++ automationTestAlgo.R
@@ -14,6 +14,8 @@ ListModelsGSL = c('gsl_ConjugateFR',"gsl_ConjugatePR", "gsl_BFGS","gsl_BFGS2","g
 
 M <- matrix(nrow = length(ListModelsGSL),ncol = N,0)
 
+computingTime <- vector(length = length(ListModelsGSL))
+
 
 # Créer le modèle
 m1 <- meanSet(Ma = c(0.3,0.8),Const = 0.1)
@@ -21,15 +23,17 @@ v1 <- varSet(Arch = list(ConstVar = 0.4, Arch = c(0.5)))
 r1 <- residualsSet('Normal')
 mod1 <- modelSet(condMean = m1, condVar = v1, condRes = r1)
 
+Param <- vector(length = N); 
+Param[1] = 0.3
+Param[2] = 0.8
+Param[3] = 0.1
+Param[4] = 0.4
+Param[5] = 0.5
+
 for(j in 1:nbSimu){
   
   # Faire une simu
   ZZ1 <- RegArchSim(nSimul = 1000, model=mod1)
-  
-  
-  
-  
-  
 
   for(i in 1:length(ListModelsGSL)){
     print(ListModelsGSL[i])
@@ -38,10 +42,11 @@ for(j in 1:nbSimu){
     Res1 <- RegArchFit(model=mod1, Yt=ZZ1$Yt,initPoint = mod1, AlgoParam = GSLAlgoParam)
     summary(Res1)
     for(k in 1:N){
-      M[i,k] =  M[i,k] + summary(Res1)$coef[k]
+      M[i,k] =  M[i,k] + (summary(Res1)$coef[k] - Param[k])^2
+
       
     }##On récupère tout ce qu'il y a à récupérer pour chaque modèle et on fait la moyenne
-    
+    computingTime[i] = computingTime[i] + Res1$GSLResult$ComputeTime
   }
   
 }
@@ -51,9 +56,12 @@ for(i in 1:length(ListModelsGSL)){
   for(k in 1:N){
     M[i,k] = M[i,k]/nbSimu
   }
+  computingTime[i] = computingTime[i]/nbSimu
 }
-
+print("Matrice d'erreur quadratiques des modèles")
 M
+print("Vecteur de temps d'execution moyen des modèles")
+computingTime
 
 
 
